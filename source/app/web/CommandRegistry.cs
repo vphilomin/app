4b2df4d2fe7c33d0da21500b7b4100a7248da31d
Roman Razilov <roman.razilov@philips.com>
2014-08-13 13:53:08 +0200

    Pushing new changes

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -1,10 +1,20 @@
-﻿namespace app.web
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace app.web
 {
   public class CommandRegistry : IFindCommandsThatCanProcessRequests
   {
+    private IEnumerable<IProcessOneRequest> commands;
+
+    public CommandRegistry(IEnumerable<IProcessOneRequest>  commands)
+    {
+      this.commands = commands;
+    }
+
     public IProcessOneRequest get_the_command_that_can_process(IProvideRequestDetails request)
     {
-      throw new System.NotImplementedException();
+      return commands.First(x => x.can_process(request));
     }
   }
 }
