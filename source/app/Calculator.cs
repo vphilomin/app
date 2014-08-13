e8b10cca124ab35046f31d465a07d0a1b0f2b848
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-08-13 03:57:39 -0600

    Updated

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -32,6 +32,7 @@ namespace app
 
     public void shut_off()
     {
+      // Small change
         if (Thread.CurrentPrincipal.IsInRole("blah"))
             return;
         throw new SecurityException("Not allowed");
