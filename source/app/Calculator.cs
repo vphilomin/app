4ee057551936314c538209d48b2d919f46712f1a
iainm42 <i.millns@philips.com>
2014-08-13 11:00:03 +0200

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Data;
 
 namespace app
 {
   public class Calculator
   {
-    public int add(int i, int i1)
+      private readonly IDbConnection _connection;
+
+      public Calculator(IDbConnection connection)
+      {
+          _connection = connection;
+      }
+
+      public int add(int i, int i1)
     {
         if (i < 0 || i1 < 0)
             throw new ArgumentException("Negative numbers not allowed");
+
+        _connection.Open();
         return i + i1;
     }
   }
