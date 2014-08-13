cc10a5ee0669eb2eae42fce98e55fc9b979945b2
vphilomin <vphilomin@gmail.com>
2014-08-13 11:30:03 +0200

    readding packages

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,35 +1,44 @@
 ﻿using System;
 using System.Data;
+using System.Security.Principal;
 
 namespace app
 {
-  public class Calculator
-  {
-    IDbConnection connection;
-
-    public Calculator(IDbConnection connection)
+    public class Calculator
     {
-      this.connection = connection;
+        IDbConnection connection;
+        IPrincipal principal;
+
+        public Calculator(IDbConnection connection, IPrincipal principal)
+        {
+            this.connection = connection;
+            this.principal = principal;
+        }
+
+        public Calculator(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public int add(int i, int i1)
+        {
+            if (i < 0 || i1 < 0)
+                throw new ArgumentException("Negative numbers not allowed");
+
+            using (connection)
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            return i + i1;
+        }
+
+        public void shut_off()
+        {
+            if (principal.IsInRole("blah"))
+                return;
+        }
     }
-
-    public int add(int i, int i1)
-    {
-      if (i < 0 || i1 < 0)
-        throw new ArgumentException("Negative numbers not allowed");
-
-      using(connection)
-      using (var command = connection.CreateCommand())
-      {
-        connection.Open();
-        command.ExecuteNonQuery();
-      }
-
-      return i + i1;
-    }
-
-    public void shut_off()
-    {
-      throw new NotImplementedException();
-    }
-  }
 }
