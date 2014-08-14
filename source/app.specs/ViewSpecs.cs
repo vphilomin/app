b3abb401c56bd7531ef5c65bbf116c7d7ba42108
Eric Beragg <eric.beragg@philips.com>
2014-08-14 14:44:08 +0200

    Pushing new changes

---
+++ source/app.specs/ViewSpecs.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using app.web.application.catalog_browsing;
+using app.web.core;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
+
+namespace app.specs
+{
+    [Subject(typeof(View<ProductsInDepartmentInput, ProductInfoItem>))]
+    public class ViewSpecs
+    {
+        public abstract class concern : Observes<ISupportAUserStory,
+          View<ProductsInDepartmentInput, ProductInfoItem>>
+        {
+        }
+
+        public class when_run : concern
+        {
+            Establish c = () =>
+            {
+                display_engine = depends.on<IDisplayInformation>();
+                product_finder = depends.on<IFind<ProductsInDepartmentInput, ProductInfoItem>>();
+                input = new ProductsInDepartmentInput();
+                request = fake.an<IProvideRequestDetails>();
+                request.setup(x => x.map<ProductsInDepartmentInput>()).Return(input);
+                products = new List<ProductInfoItem>();
+
+                product_finder.setup(x => x.get(input)).Return(products);
+            };
+
+            Because b = () =>
+              sut.process(request);
+
+            It displays_the_products_of_the_chosen_department = () =>
+              display_engine.received(x => x.display(products));
+
+            static IFind<ProductsInDepartmentInput, ProductInfoItem> product_finder;
+            static IProvideRequestDetails request;
+            static IDisplayInformation display_engine;
+            static IEnumerable<ProductInfoItem> products;
+            static ProductsInDepartmentInput input;
+        }
+    }
+}
---
+++ source/app/web/application/catalog_browsing/IFind.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace app.web.application.catalog_browsing
+{
+    public interface IFind<in Input, out Output>
+    {
+        IEnumerable<Output> get(Input input);
+    }
+}
